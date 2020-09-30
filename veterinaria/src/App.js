91dae95fb3263fcf5eb2ea03b166a746ca2ccbfb
tomp <you@example.com>
2020-09-30 01:12:10 -0300

    Formulario funcional

--- veterinaria/src/App.js
+++ veterinaria/src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState }from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
+
 function App() {
+
+const [citas, guardarCitas] = useState([]);
+
+const crearCita = cita => {
+  guardarCitas([
+    ...citas,
+    cita
+  ])
+};
+
+const eliminarCita = id => {
+const citasFilter = citas.filter(cita => cita.id !== id);
+guardarCitas(citasFilter)
+};
+
+const tituloCitas = citas.length === 0 ? "Sin citas" : "Administra tus citas";
+
   return (
     <div className="App">
       <h1>Clinca Veterinaria</h1>
@@ -13,7 +33,20 @@ function App() {
           <div className="one-half column">
             2
           </div>
-          <Formulario/>
+          <Formulario
+            crearCita={crearCita}
+            />
+        </div>
+        
+        <div className="one-half column">
+          <h2>{tituloCitas}</h2>
+          {citas && citas.map(cita => (
+            <Cita 
+            cita={cita}
+            key={cita.id}
+            eliminarCita={eliminarCita}
+            />
+          ))}
         </div>
       </div>
     </div>
--- veterinaria/src/components/Formulario.js
+++ veterinaria/src/components/Formulario.js
@@ -1,37 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-const Formulario = () => {
+
+const Formulario = ({ crearCita }) => {
+
+  const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+  });
+
+  const [error, actualizarError] = useState(false)
+
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+  
+  const submitcita = (e) => {
+    e.preventDefault();
+
+    //Validación de campos vacios
+    if(mascota.trim() === "" || propietario.trim() === "" || fecha.trim() === "" || 
+    hora.trim() === "" || sintomas.trim() === "") {
+      return actualizarError(true)
+    } 
+      actualizarError(false)
+    
+
+    //ID por cita usando uuid v4
+    cita.id = uuidv4();
+
+    //Creo la cita
+    crearCita(cita)
+
+    //Borrar campos una vez enviado
+    actualizarCita({
+      mascota: '',
+      propietario: '',
+      fecha: '',
+      hora: '',
+      sintomas: ''
+    })
+  }
+
+  
+  
   return (
-    <div>
+    <div className="container">
       <h2> Crear Cita </h2>
 
-      <form>
+      { error ? <p className="alerta-error">Requerimos que llenes todos los campos</p> 
+      : null }
+
+      <form
+      onSubmit={submitcita}
+      >
         <label>Nombre Mascota</label>
         <input
+          onChange={actualizarState}
           type="text"
           name="mascota"
           className="u-full-width"
           placeholder="Nombre de la mascota"
+          value={mascota}
         />
 
         <label>Nombre Mascota</label>
         <input
+          onChange={actualizarState}
           type="text"
           name="propietario"
           className="u-full-width"
           placeholder="Nombre del dueño de la mascota"
+          value={propietario}
         />
 
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input 
+        onChange={actualizarState}
+        type="date" 
+        name="fecha" 
+        className="u-full-width" 
+        value={fecha}
+        />
 
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input 
+        onChange={actualizarState}
+        type="time" 
+        name="hora" 
+        className="u-full-width" 
+        value={hora}
+        />
       
         <label>Sintomas</label>
         <textarea
+          onChange={actualizarState}
           name="sintomas"
           className="u-full-width"
+          value={sintomas}
         ></textarea>
 
         <button
